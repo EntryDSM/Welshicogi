22066773c09364a8ea07dc56e03ce02cf79ec95e
junukim <junukim.dev@gmail.com>
2020-08-24 20:56:47 +0900

    [UPDATE] 소켓 토큰 연결 수정

--- app/src/components/main/index.tsx
+++ app/src/components/main/index.tsx
@@ -1,11 +1,27 @@
 import React, { FC, useEffect, useRef } from "react";
 
+import { authentication } from "middleware/api/socket";
 import { getItemToSesstion } from "utils/stroage";
 import Login from "../login";
 import Chat from "../chat";
 import * as S from "./style";
 
 const Main: FC = () => {
+  const didMountRef = useRef(false);
+
+  useEffect(() => {
+    if (!didMountRef.current) {
+      didMountRef.current = true;
+
+      if (getItemToSesstion("access_token")) {
+        authentication({
+          token: getItemToSesstion("access_token"),
+          type: "admin",
+        });
+      }
+    }
+  }, [didMountRef]);
+
   return (
     <S.Wrapper>
       {getItemToSesstion("access_token") ? <Chat /> : <Login />}
